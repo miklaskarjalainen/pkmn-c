d9090e2b78d2a8658e6c32b0df92e968b3aeb0cc
Giffi <giffidev@gmail.com>
2024-08-06 00:00:58 +0300

    null check

--- src/pkmn_move.c
+++ src/pkmn_move.c
@@ -4,6 +4,10 @@
 #include "pkmn_rand.h"
 
 pkmn_move_t pkmn_move_from_semantic(const pkmn_move_semantics_t* semantic) {
+    if (!semantic) {
+        return (pkmn_move_t) { NULL };
+    }
+
     return (pkmn_move_t) {
         .move = semantic,
         .pp_left = semantic->base_pp,

64d267d58c950625472b3781d52bb748f7542f09
Giffi <giffidev@gmail.com>
2024-08-06 04:07:20 +0300

    custom random number generator
    the final goal is to not having to link with LIBC so this helps with that.

--- src/pkmn_rand.c
+++ src/pkmn_rand.c
@@ -1,30 +1,37 @@
-// TODO: Write a custom random machine.
-
-#include <stdlib.h>
-
 #include "pkmn_rand.h"
 
+// RANDOM BASED ON https://tasvideos.org/GameResources/GBA/PokemonGen3/RNG
+static uint32_t sRandomSeed = 0x0; 
+
 void pkmn_init_seed(uint32_t seed) {
-    srand(seed);
+    sRandomSeed = seed;
 }
 
-uint8_t  pkmn_rand_u8(void) {
-    return (uint8_t)rand();
+uint8_t pkmn_rand_u8(void) {
+    return (uint8_t)pkmn_rand_u32();
+}
+
+uint16_t pkmn_rand_u16(void) {
+    return (uint16_t)pkmn_rand_u32();
 }
 
 uint32_t pkmn_rand_u32(void) {
-    return (uint32_t)rand();
+    const uint32_t MULTIPLIER = 0x41C64E6D;
+    const uint32_t ADDER = 0x6073;
+    sRandomSeed = sRandomSeed * MULTIPLIER + ADDER;
+    return sRandomSeed;
 }
 
 int32_t pkmn_rand_i32(void) {
-    return (int32_t)rand();
+    uint32_t res = pkmn_rand_u32();
+    return *(int32_t*)&res;
 }
 
 int32_t pkmn_rand_range(int32_t min, int32_t max) {
-    return pkmn_rand_i32() % (max + 1 - min) + min;
+    return pkmn_rand_u32() % (max + 1 - min) + min;
 }
 
 float pkmn_randf(void) {
-    return (float)pkmn_rand_range(0, 65536) / 65536.0f;
+    return (float)pkmn_rand_u16() / 0xFFFF;
 }
 
